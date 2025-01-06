33e8010f20491a95c4ae64f7f8c3064481dfa74d
Ravindra <rohitmishra27112005@gmail.com>
2025-01-06 11:20:47 +0530

    changed the user name

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "headings" }, "Hello World from React")
-  )
-);
+import { log } from "console";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading =
+// React Element
+
+const heading = React.createElement("h1", {}, "This is main Heading");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
+
+let a;
+console.log(a);
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement(
+//       "h1",
+//       { id: "headings" },
+//       "Hello World from React with server "
+//     )
+//   )
+// );
+
+// // const heading =
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# parcel
+
+- dev builds
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Consistent Hashing
+- Compressing
+- Code Splitting
+- Differential Bundling - To support folder browser
+- Diagnostics
+- Better Error Handling
+- HTTPS
+- Different Build for dev and production bundle
